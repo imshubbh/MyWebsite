66328fbd5b42474b787c2861c29de17346ce5076
shubham singh <singhshubham765@gmail.com>
2022-05-22 14:12:09 +0530

    adding url for calc

--- urls.py
+++ urls.py
@@ -9,6 +9,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('calculator/', views.calcPage, name='calc'),
     path('', include('shortner.urls')),
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
