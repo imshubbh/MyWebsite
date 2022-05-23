6be93ac525baa3fa6345acd5f512d1517bac494c
shubham singh <singhshubham765@gmail.com>
2022-05-23 13:36:47 +0530

    fixing calc url

--- shortner/urls.py
+++ shortner/urls.py
@@ -4,6 +4,7 @@ from shortner import views
 
 urlpatterns = [
     path('<str:pk>/', views.go, name='create'),
+    path('calculator/', views.calcPage, name='calc'),
     path('', views.homePage, name='Home' ),
     
 ]
