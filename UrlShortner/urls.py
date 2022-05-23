8a4ced8fcdb0c26bf9ab112af64457ce31301368
shubham singh <singhshubham765@gmail.com>
2022-05-23 13:49:28 +0530

    fixing calc url

--- UrlShortner/urls.py
+++ UrlShortner/urls.py
@@ -11,7 +11,7 @@ from shortner import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('calculator/', views.calcPage, name='calc'),
+    # path('calculator/', views.calcPage, name='calc'),
     path('', include('shortner.urls')),
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
